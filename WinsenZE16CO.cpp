e23099bd2b2285dc09a02c66dd5d1857d40dd92f
Vladimir Kozlov <vkozlov@tacitknowledge.com>
2019-03-14 11:10:13 +0200

    Initial implementation

---
+++ WinsenZE16CO.cpp
@@ -0,0 +1,81 @@
+#include <WinsenZE16CO.h>
+
+WinsenZE16CO::WinsenZE16CO(Stream * stream)
+{
+	_stream = stream;
+}
+
+bool WinsenZE16CO::readUntil(unsigned long * result, uint16_t timeout)
+{
+    _data = result;
+    uint32_t start = millis();
+    do
+    {
+      readLoop();
+      if (_status == STATUS_OK) break;
+    } while (millis() - start < timeout);
+
+    return _status == STATUS_OK;
+}
+
+void WinsenZE16CO::readLoop()
+{
+	_status = STATUS_WAITING;
+	if (_stream->available())
+	{
+		uint8_t ch = _stream->read();
+		
+		Serial.print(" [");Serial.print(_index);Serial.print("]=");Serial.print(ch, HEX);
+		
+		switch (_index)
+	    {
+	    	case 0:
+	      	if (ch != 0xFF)
+	      	{
+	        	return;
+	      	}
+	      	_calculatedChecksum = 0;
+	      	break;
+	    case 1:
+	      	if (ch != 0x04)
+	      	{
+	        	_index = 0;
+	        	return;
+	      	}
+	      	_calculatedChecksum += ch;
+	      	break;
+
+	    default:
+	      	if (_index < 8)
+	      	{
+	        	_calculatedChecksum += ch;
+	      	}
+	      	else
+	      	{
+	        	_checksum = ch;
+	      	}
+	      	break;
+	    } // switch
+
+	    if (_index == 8)
+	    {
+	        _calculatedChecksum = 0xFF - _calculatedChecksum + 1;
+
+	        *_data = _payload[4] * 256 + _payload[5];
+
+	        if (_calculatedChecksum == _checksum)
+	        {
+	          _status = STATUS_OK;
+	        }
+
+	        _index = 0;
+	        return;
+	    }
+	    else
+	    {
+	      	_payload[_index] = ch;
+	    }
+ 
+	    _index++;
+	}
+}
---
+++ WinsenZE16CO.h
@@ -0,0 +1,30 @@
+#ifndef WinsenZE16CO_H_
+#define WinsenZE16CO_H_
+
+#if ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class WinsenZE16CO
+{
+private:
+	Stream * _stream;
+	uint8_t _index = 0;
+	enum STATUS { STATUS_WAITING, STATUS_OK };
+	uint8_t _payload[9];
+	uint16_t _checksum;
+	uint16_t _calculatedChecksum;
+	double * _data;
+	STATUS _status;
+	void readLoop();
+public:
+	WinsenZE16CO(Stream * stream);
+	bool readUntil(unsigned long * result, uint16_t timeout);
+};
+
+
+
+
+#endif
